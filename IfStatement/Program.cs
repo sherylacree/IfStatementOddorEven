3dbe6863ec589a573e48d743535be0d14ef2c6e9
sheryl acree <sacree@tql.com>
2024-06-12 14:32:48 -0400

    Add project files.

---
+++ IfStatement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace IfStatement
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.Write("Enter an integer:");
+                string answer = Console.ReadLine();
+
+
+                int aNumber = Convert.ToInt32(answer);
+
+                if (aNumber == 0)
+                {
+                    break;
+                }
+                if (aNumber % 2 == 0)
+                {
+                    Console.WriteLine($"{aNumber} is even.");
+                }
+                else { Console.WriteLine($"{aNumber} is odd."); }
+            }
+
+
+
+        }
+    }
+}
+
+
